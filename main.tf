1b05d0acf866d936659400377826fe8dc22e721c
Abhinaya-45 <abhinaya.venugopal20@gmail.com>
2025-09-10 21:35:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,33 @@
 resource "aws_s3_bucket" "mywebpage" {
-
-   bucket = var.bucketname
-   
+   bucket = var.bucketname 
 }
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+bucket = aws_s3_bucket.mywebpage.id
+
+rule {
+object_ownership = "BucketOwnerPreferred"
+}
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+bucket = aws_s3_bucket.mywebpage.id
+
+block_public_acls = false
+block_public_policy = false
+ignore_public_acls = false
+restrict_public_buckets = false
+
+}
+
+resource "aws_s3_bucket_acl" "example" {
+
+depends_on = [
+aws_s3_bucket_ownership_controls.example
+aws_s3_bucket_public_access_block.example
+]
+
+bucket =  aws_s3_bucket.mywebpage.id
+acl = public-read
+}
+
