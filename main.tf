eea51c055236caf6ebcfb193d85968669b656078
Abhinaya-45 <abhinaya.venugopal20@gmail.com>
2025-09-10 21:53:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,3 +31,40 @@ bucket =  aws_s3_bucket.mywebpage.id
 acl = public-read
 }
 
+resource "aws_s3_object" "index" {
+
+bucket = aws_s3_bucket.mywebpage.id
+key = "index.html"
+source = "index.html"
+acl = "public-read"
+content_type = "text/html"
+
+}
+
+resource "aws_s3_object" "error" {
+
+bucket = aws_s3_bucket.mywebpage.id
+key = "error.html"
+source = "error.html"
+acl = "public-read"
+content_type = "text/html"
+
+}
+
+resource "aws_s3_bucket_website_configuration" "example" {
+
+bucket = aws_s3_bucket.mywebpage.id
+
+index_bucket {
+
+suffix = "index.html"
+}
+
+error_bucket {
+
+key = "error.html"
+}
+
+depends_on [ aws_s3_bucket_acl.example ]
+
+}
